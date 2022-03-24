2ab2a0af23ac60ac1a7e0b6ff2676506877dd3cf
tabakov <gnt137@gmail.com>
2022-03-24 20:04:27 +0200

    Post Office & Rage Quit solutions added

---
+++ More Exercise Regular Expressions/Post Office/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Post_Office
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|").ToArray();
+
+            string firstPart = input[0];
+            string secondPart = input[1];
+            string thirdPart = input[2];
+
+            string patternFirstP = @"([\$\#\%\*\&])(?<letters>[A-Z]+)\1";
+            string patternSecondP = @"\d+\:\d{2}";
+
+            string[] words = thirdPart.Split();
+
+
+            Match capitalLetters = Regex.Match(firstPart, patternFirstP);
+            MatchCollection wordsData = Regex.Matches(secondPart, patternSecondP);
+
+            foreach (char ch in capitalLetters.Groups["letters"].Value)
+            {
+                int asciiValue = ch;
+
+                foreach (Match item in wordsData)
+                {
+                    if (int.Parse(item.Value.Split(":")[0]) == asciiValue)
+                    {
+                        int wordLength = int.Parse(item.Value.Split(":")[1])+1;
+
+                        string[] filteredWords = words.Where(word => word.Length == wordLength && word[0]==ch).ToArray();
+
+                        if (filteredWords.Length > 0)
+                        {
+                            Console.WriteLine(filteredWords[0]);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ More Exercise Regular Expressions/Rage Quit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+
+namespace Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            string pattern = @"([\D]+\d+)+?";
+            string patternForDistinct = @"\D";
+
+            MatchCollection commands = Regex.Matches(input, pattern);
+            MatchCollection uniqueSimb = Regex.Matches(input, patternForDistinct);
+
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (Match match in commands)
+            {
+                int iterations = int.Parse(Regex.Match(match.Value, @"\d+").Value);
+                string strToRepeat = Regex.Match(match.Value, @"\D+").Value;
+
+                result.Append(new StringBuilder(strToRepeat.Length * iterations).Insert(0, strToRepeat, iterations).ToString().ToUpper());
+            }
+
+            int uniqueS = result.ToString().Select(x => x.ToString().ToLower()).Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {uniqueS}");
+            Console.WriteLine(result);
+        }
+    }
+}
