ad10ca93c8869c47a55f61f7660bd0e04e1cd379
tabakov <gnt137@gmail.com>
2022-03-24 20:25:50 +0200

    ..

---
+++ More Exercise Regular Expressions/Santa's Secret Helper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            List<string> goodKids = new List<string>();
+            string encriptedMsg = Console.ReadLine();
+
+            while (encriptedMsg != "end")
+            {
+                string decriptedMsg = string.Empty;
+
+                foreach (char ch in encriptedMsg)
+                {
+                    decriptedMsg += (char)(ch - num);
+                }
+
+                string pattern = @"\@(?<name>[A-Za-z]+)[^\@\-\!\:\>]*\!(?<behaivior>[GN])\!";
+                Match validInput = Regex.Match(decriptedMsg, pattern);
+
+                if (validInput.Success && validInput.Groups["behaivior"].Value == "G")
+                {
+                    goodKids.Add(validInput.Groups["name"].Value);
+                }
+
+                encriptedMsg = Console.ReadLine();
+
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, goodKids));
+
+        }
+    }
+}
