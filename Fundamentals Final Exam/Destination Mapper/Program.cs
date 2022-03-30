ff0ceb35457d412b26348aa410281abd3ac5c99e
tabakov <gnt137@gmail.com>
2022-03-30 16:31:49 +0300

    .

--- Fundamentals Final Exam/Destination Mapper/Program.cs
+++ Fundamentals Final Exam/Destination Mapper/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Destination_Mapper
 {
@@ -6,7 +9,24 @@ namespace Destination_Mapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string input = Console.ReadLine();
+
+            string pattern = @"([\=\/])(?<location>[A-Z][A-Za-z]{2,})\1";
+            MatchCollection validLocations = Regex.Matches(input, pattern);
+            List<string> validL = new List<string>();
+            int travelPoints = 0;
+
+            foreach (Match match in validLocations)
+            {
+                validL.Add(match.Groups["location"].Value);
+                travelPoints += match.Groups["location"].Value.Length;
+
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", validL)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+
         }
     }
 }
